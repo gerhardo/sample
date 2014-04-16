421d52f9a32a9704b49235870aa1cbe8ad1e6776
go <go@go-laptop>
2014-04-16 20:45:39 +0200

    Logging mit LOG4J 1.2

---
+++ JettyLog4j/src/main/java/de/emediapark/sample/jettylog4j/HelloWorld.java
@@ -0,0 +1,39 @@
+package de.emediapark.sample.jettylog4j;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+ 
+public class HelloWorld extends AbstractHandler
+{
+	private static final Logger LOG = Logger.getLogger(HelloWorld.class);
+	
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response) 
+        throws IOException, ServletException
+    {
+    	LOG.info("handle enter");
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
+    	LOG.info("handle done");
+    }
+ 
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(8080);
+        server.setHandler(new HelloWorld());
+ 
+        server.start();
+        server.join();
+    }
+}
