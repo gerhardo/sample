6b6c7a6fb3b2461a4c08921f10f1cdf6505d3756
geotte <geotte@CE18362.corp.capgemini.com>
2018-08-20 11:54:33 +0200

    with swagger and api generation

--- FileReceiver/src/main/java/sample/file/api/FileReceiverController.java
+++ FileReceiver/src/main/java/sample/file/api/FileReceiverController.java
@@ -9,39 +9,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import sample.file.domain.FileReceiver;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @RestController
 @RequestMapping("/")
+@EnableSwagger2
+@Api(value = "/FileReceiverController", tags = "filereceiver-api", description = "filereceiver-api-operations")
 public class FileReceiverController {
 
 	private static final Logger LOG = Logger.getLogger(FileReceiverController.class.getName());
 
-	@PostMapping(path ="upload/{filename}", consumes="application/octet-stream")
-	public ResponseEntity<String> uploadFile(@PathVariable("filename") String filename,
-            InputStream fileInputStream) {
+	@PostMapping(path = "upload/{filename}", consumes = "application/octet-stream")
+	@ApiOperation(value = "upload file")
+	public ResponseEntity<String> uploadFile(@PathVariable("filename") String filename, InputStream fileInputStream) {
 
 		try {
 			LOG.fine("writing of " + filename + " started");
 			FileReceiver fr = new FileReceiver();
 			fr.saveInputToDirectory(filename, fileInputStream);
 			LOG.info("upload of " + filename + " done");
-			return new ResponseEntity<String>("uploaded " + filename,HttpStatus.OK);
-		}
-		catch (Exception e) {
+			return new ResponseEntity<String>("uploaded " + filename, HttpStatus.OK);
+		} catch (Exception e) {
 			LOG.severe("exception receiving file: " + e.toString());
 			return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
 		}
 	}
 
-
 	@GetMapping("health-check")
 	public ResponseEntity<String> areYouThere() {
 		return new ResponseEntity<String>("file receiver running", HttpStatus.OK);
 	}
-
 
 }
